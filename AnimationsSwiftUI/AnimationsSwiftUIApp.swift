7e92186538fedcab65aecb996c8bfac9593545e1
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-17 23:43:03 +0300

    Initial Commit

---
+++ AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by Ömer on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsSwiftUITests/AnimationsSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUITests.swift
+//  AnimationsSwiftUITests
+//
+//  Created by Ömer on 17.01.2025.
+//
+
+import Testing
+@testable import AnimationsSwiftUI
+
+struct AnimationsSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
